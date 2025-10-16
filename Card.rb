6618f7cc5ae2eb0def340a77a85d500bc71766e5
Ticosmiley <henry.m.hazlett@gmaill.com>
2025-10-16 15:42:01 -0500

    adding comments

--- Card.rb
+++ Card.rb
@@ -2,15 +2,15 @@ require "rmagick"
 
 class Card
     def initialize(name = '', manaCost = '', type = '', subType = '', rarity = '', rules = '', flavor = '', stats = '', art = '')
-        @name = name
-        @manaCost = manaCost
-        @type = type
-        @subType = subType
-        @rarity = rarity
-        @rules = rules
-        @flavor = flavor
-        @stats = stats
-        @art = art
+        @name = name                     # String, ex: 'Krark, the Thumbless'
+        @manaCost = manaCost             # String of mana values, ex: '(1)(R)' for 1 generic mana and one red mana
+        @type = type                     # String, most typically one of the following: 'Creature', 'Sorcery', 'Instant', 'Enchantment', 'Artifact'
+        @subType = subType               # String, often left blank, used most often with creatures, enchantments, and artifacts, ex: 'Goblin Warrior'
+        @rarity = rarity                 # String, only one of the following: 'Common', 'Uncommon', 'Rare', 'Mythic'
+        @rules = rules                   # String, ex: 'Whenever you cast an instant or sorcery spell, flip a coin. If you lose the flip, return that spell to its owner’s hand. If you win the flip, copy that spell, and you may choose new targets for the copy.'
+        @flavor = flavor                 # String, ex: 'Double or nothing.'
+        @stats = stats                   # String of the format power/toughness, ex: '2/2'
+        @art = art                       # String, relative filepath of art to be used
     end
 
     def displayCard
