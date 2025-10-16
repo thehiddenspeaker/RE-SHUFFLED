b5e76cf5c4269a5e982121e59ee807522b88bc78
Ticosmiley <henry.m.hazlett@gmaill.com>
2025-10-15 23:23:22 -0500

    Initial card class

---
+++ Card.rb
@@ -0,0 +1,35 @@
+require "rmagick"
+
+class Card
+    def initialize(name = '', manaCost = '', type = '', subType = '', rarity = '', rules = '', flavor = '', stats = '', art = '')
+        @name = name
+        @manaCost = manaCost
+        @type = type
+        @subType = subType
+        @rarity = rarity
+        @rules = rules
+        @flavor = flavor
+        @stats = stats
+        @art = art
+    end
+
+    def displayCard
+        puts "Name: #{@name}\n"\
+             "Mana Cost: #{@manaCost}\n"\
+             "Type: #{@type}\n"\
+             "Subtype: #{@subType}\n"\
+             "Rarity: #{@rarity}\n"\
+             "Rules Text: #{@rules}\n"\
+             "Flavor Text: #{@flavor}\n"\
+             "Stats: #{@stats}"
+    end
+end
+
+card = Card.new('Sol Ring', '(1)', 'Artifact', '', 'Uncommon', '(T): Add (C)(C).', '"All creation in a single point, the point of all creation in all." -Sunstar loop-mantra', '')
+
+card.displayCard
+
+test = Magick::ImageList.new("Images/Sol_Ring.webp")
+tempFile = "Images/tempfile.webp"
+test.write(tempFile)
+system("start #{tempFile}")
